25734e5f195a14159b43412303b7c7692e58b937
James Nicholls <james@kert.com.au>
2025-11-05 08:43:23 +1000

    smooth out a few formatting and comment clarity issues

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from pydantic import BaseModel
 from dotenv import load_dotenv
 
@@ -9,6 +11,15 @@ from routes.chats import chats as chats_router
 from routes.websocket import router as websocket_router
 
 app = FastAPI()
+
+# add CORS middlware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # ONLY DURING DEVELOPMENT - LIMIT FOR PRODUCTION    
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows GET, POST, PUT, DELETE, etc.
+    allow_headers=["*"]
+)
 
 from database.database import engine
 from database.models import Base
@@ -23,3 +34,6 @@ app.include_router(sessions_router)
 app.include_router(chats_router)
 app.include_router(websocket_router)
 
+@app.get("/")
+def root():
+    return {"message": "Chatroom API"}
